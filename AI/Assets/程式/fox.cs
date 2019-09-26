247f41e9200989204dc589cab3a67c54aa3a2dbb
Daphny-Chen <55218517+Daphny-Chen@users.noreply.github.com>
2019-09-26 11:09:41 +0800

    FOX

---
+++ AI/Assets/程式/fox.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;      //引用UnityAPTT-API倉庫功能、工具
+
+public class fox : MonoBehaviour    //類別 類別名稱
+{
+    //成員:欄位、屬性、方法、事件
+
+    //事件:在特定時間點會已指定頻率執行的方法
+    //開始事件:遊戲開始時執行一次
+    private void Start()
+    {
+
+
+    }
+
+
+
+
+}
