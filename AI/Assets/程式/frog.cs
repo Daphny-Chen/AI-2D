06ac95f847c42beb704eb5067914852e76c79043
Daphny-Chen <55218517+Daphny-Chen@users.noreply.github.com>
2019-10-03 11:59:48 +0800

    1003NPC設定

---
+++ AI/Assets/程式/frog.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class frog : MonoBehaviour {
+
+
+
+
+    [Header("對話")]
+    public string SayStart = "  ";
+    public string SayNotComplete = "  ";
+    public string SayComplete = "  ";
+    [Header("對話速度")]
+    public float Speed = 1.5f;
+    [Header("任務相關")]
+    public bool Complete = false;
+    public int CountPlayer = 0;
+    public int CountFinish = 10;
+
+    // Use this for initialization
+    void Start () {
+		
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+}
